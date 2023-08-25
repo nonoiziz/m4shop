01e31415851a2f463b62a68a84da5d285d2888c6
Matthew <minhsiutsai@gmail.com>
2023-08-25 12:12:24 +0800

    build: Cart.jsx set up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,18 +5,29 @@ import Step1 from './Step1'
 import Step2 from './Step2'
 import Step3 from './Step3'
 import ProgressControl from './ProgressControl'
-
+import Cart from './Cart'
 
 function App() {
   return (
   <div className="container">
     <Header />
-    <div className="info-container">
+    <div className='main-container'>
+      <div className="stepProgress-container">
       <StepProgress />
-      <Step1 />
-      <Step2 />
-      <Step3 />
-      <ProgressControl />
+      </div>
+      <div className="info-container">
+        
+        <div className="left-container">
+         <Step1 />
+         <Step2 />
+         <Step3 /> 
+         <ProgressControl />  
+        </div>
+        <div className="right-container">
+         <Cart />
+        </div>
+      </div>
+      
     </div>
   </div>
   )
---
+++ src/components/Cart.jsx
@@ -0,0 +1,9 @@
+import './scss_dir/Cart.scss'
+
+function Cart() {
+  return (
+    <h1>hi</h1>
+  )
+}
+
+export default Cart
