1afe1d900b5825f81f613f99fd741a08803afd39
Matthew <minhsiutsai@gmail.com>
2023-09-05 23:38:37 +0800

    build: Cart button build

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,26 +1,66 @@
 import './scss_dir/Cart.scss'
-import {product} from './data/product-data'
+import { product } from './data/product-data'
+import { useState } from 'react';
 
 
-function Cart() {
-  const listItems = product.map( item =>
+
+export default function Cart() {
+  const [ products, setProducts ] = useState([...product])
+  
+  function handleIncreaseClick (productId) {
+    setProducts(products.map((product) => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            quantity: product.quantity + 1
+          };
+        } else {
+          return product
+        }
+      }))
+  }
+    
+  function handleDecreaseClick (productId) {
+    setProducts(products.map((product) => {
+      if (product.id === productId) {
+        return {
+          ...product,
+          quantity: (product.quantity > 0 ? product.quantity - 1 : product.quantity)
+        } 
+      } else {
+          return product
+        }
+    }))
+  }
+
+  function totalPrice() {
+    let total = 0
+    products.forEach((product) => {
+      total += product.price * product.quantity
+    })
+    
+      return (total < 1200 ? total + 450 : total)
+  }
+  
+  
+  const listItems = products.map( item =>
     <div className="products-container">
-      <div className="product d-flex" key={item.id}>
+      <div className="product d-flex ml" key={item.id}>
         <div className="product-image">
           <img src={item.img} className="product-avatar" />
         </div>
         <div className="product-set d-flex">
           <div className="product-info d-flex">
-            <div className="product-name">{item.name}</div>
-            <div className="product-price">{item.price}</div>
+            <div className="product-name ml-5">{item.name}</div>
+            <div className="product-price">{item.price * item.quantity}</div>
           </div>
       
           <div className="buyer-action d-flex">
             <div className="circle-container">
-            <button className="mount-reduce" id='mount-reduce'>-</button></div>
-            <div className="mount-counter">5</div>
+            <button onClick={() => {handleDecreaseClick(item.id)}} className="mount-reduce" id='mount-reduce'>-</button></div>
+            <div className="mount-counter">{item.quantity}</div>
             <div className="circle-container">
-            <button className="mount-add" id="mount-add">+</button></div>
+            <button onClick={() => {handleIncreaseClick(item.id);}} className="mount-add" id="mount-add">+</button></div>
           </div>
         </div>
       </div>
@@ -37,10 +77,9 @@ function Cart() {
       </div>
       <div className="totalprice-container d-flex">
         <div className="totalprice-title">小計</div>
-        <div className="totalprice">$5298</div>
+        <div className="totalprice">${totalPrice()}</div>
       </div>
       </section>
   </>;
 }
 
-export default Cart
